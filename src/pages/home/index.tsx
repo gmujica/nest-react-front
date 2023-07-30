7d346f635458f1989bf56e3af79cf3c370496c04
gmujica <gregorymujica1993@gmail.com>
2023-07-29 22:59:53 -0300

    adding some dummy data for test the card component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,130 +5,17 @@ import { CardComponent, HeaderComponent } from "../../components";
 import { characters } from "../../api/characters";
 import { EventInterface } from "./interface/event.interface";
 import { events } from "../../api/events";
+import { dymmyData } from "./dummyData";
 
 export const HomePage: FC<{}> = (Props) => {
   const [allEvents, setAllEvents] = useState<EventInterface[] | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
 //
-  const [dummyEvents, setDummyEvents] = useState([
-    {
-      "event_id": "ae77987d-e6ea-48e7-8e03-bf7c9f0d6684",
-      "title": "event11111",
-      "descrption": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-      "created_at": "2023-07-27T02:14:25.835Z",
-      "updated_at": "2023-07-27T02:14:25.835Z",
-      "user": {
-        "id": "f3e3d373-f924-4dbe-b19e-ca21a773a2df",
-        "name": "bbb",
-        "email": "bbb@mail.com",
-        "created_at": "2023-07-27T02:14:25.835Z",
-        "updated_at": "2023-07-27T02:14:25.835Z"
-      }
-    },
-    {
-      "event_id": "ae77987d-e6ea-48e7-8e03-bf7c9f0d6684",
-      "title": "event11111",
-      "descrption": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-      "created_at": "2023-07-27T02:14:25.835Z",
-      "updated_at": "2023-07-27T02:14:25.835Z",
-      "user": {
-        "id": "f3e3d373-f924-4dbe-b19e-ca21a773a2df",
-        "name": "bbb",
-        "email": "bbb@mail.com",
-        "created_at": "2023-07-27T02:14:25.835Z",
-        "updated_at": "2023-07-27T02:14:25.835Z"
-    }  },
-      {
-        "event_id": "ae77987d-e6ea-48e7-8e03-bf7c9f0d6684",
-        "title": "event11111",
-        "descrption": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-        "created_at": "2023-07-27T02:14:25.835Z",
-        "updated_at": "2023-07-27T02:14:25.835Z",
-        "user": {
-          "id": "f3e3d373-f924-4dbe-b19e-ca21a773a2df",
-          "name": "bbb",
-          "email": "bbb@mail.com",
-          "created_at": "2023-07-27T02:14:25.835Z",
-          "updated_at": "2023-07-27T02:14:25.835Z"
-        },  
-    },
-    {
-      "event_id": "ae77987d-e6ea-48e7-8e03-bf7c9f0d6684",
-      "title": "event11111",
-      "descrption": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-      "created_at": "2023-07-27T02:14:25.835Z",
-      "updated_at": "2023-07-27T02:14:25.835Z",
-      "user": {
-        "id": "f3e3d373-f924-4dbe-b19e-ca21a773a2df",
-        "name": "bbb",
-        "email": "bbb@mail.com",
-        "created_at": "2023-07-27T02:14:25.835Z",
-        "updated_at": "2023-07-27T02:14:25.835Z"
-      },  
-    },
-    {
-      "event_id": "ae77987d-e6ea-48e7-8e03-bf7c9f0d6684",
-      "title": "event11111",
-      "descrption": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-      "created_at": "2023-07-27T02:14:25.835Z",
-      "updated_at": "2023-07-27T02:14:25.835Z",
-      "user": {
-        "id": "f3e3d373-f924-4dbe-b19e-ca21a773a2df",
-        "name": "bbb",
-        "email": "bbb@mail.com",
-        "created_at": "2023-07-27T02:14:25.835Z",
-        "updated_at": "2023-07-27T02:14:25.835Z"
-      },  
-    },
-    {
-      "event_id": "ae77987d-e6ea-48e7-8e03-bf7c9f0d6684",
-      "title": "event11111",
-      "descrption": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-      "created_at": "2023-07-27T02:14:25.835Z",
-      "updated_at": "2023-07-27T02:14:25.835Z",
-      "user": {
-        "id": "f3e3d373-f924-4dbe-b19e-ca21a773a2df",
-        "name": "bbb",
-        "email": "bbb@mail.com",
-        "created_at": "2023-07-27T02:14:25.835Z",
-        "updated_at": "2023-07-27T02:14:25.835Z"
-      },  
-    },
-    {
-      "event_id": "ae77987d-e6ea-48e7-8e03-bf7c9f0d6684",
-      "title": "event11111",
-      "descrption": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-      "created_at": "2023-07-27T02:14:25.835Z",
-      "updated_at": "2023-07-27T02:14:25.835Z",
-      "user": {
-        "id": "f3e3d373-f924-4dbe-b19e-ca21a773a2df",
-        "name": "bbb",
-        "email": "bbb@mail.com",
-        "created_at": "2023-07-27T02:14:25.835Z",
-        "updated_at": "2023-07-27T02:14:25.835Z"
-      },  
-    },
-    {
-      "event_id": "ae77987d-e6ea-48e7-8e03-bf7c9f0d6684",
-      "title": "event11111",
-      "descrption": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-      "created_at": "2023-07-27T02:14:25.835Z",
-      "updated_at": "2023-07-27T02:14:25.835Z",
-      "user": {
-        "id": "f3e3d373-f924-4dbe-b19e-ca21a773a2df",
-        "name": "bbb",
-        "email": "bbb@mail.com",
-        "created_at": "2023-07-27T02:14:25.835Z",
-        "updated_at": "2023-07-27T02:14:25.835Z"
-      },  
-    },
-  ])
+  const [dummyEvents, setDummyEvents] = useState(dymmyData)
 //
   useEffect(() => {
     setLoading(true);
     events.getAll().then((r) => {
-    //characters.getAll({page: 1}).then((r) => {
-    //characters.getById({id: 1}).then((r) => {
       console.log('Home log', allEvents);
       setAllEvents(r.data.results)
     }).catch((e) => {
